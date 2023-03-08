2aedd03699a530ec99d05fb7f18da31b67d3d4e9
Kairo97 <109006341+kairo97@users.noreply.github.com>
2023-03-08 12:16:21 -0800

    added more psuedocode

--- src/Pages/Game.js
+++ src/Pages/Game.js
@@ -1,6 +1,11 @@
 
 import React from 'react';
 import './Game.css';
+// TODO: create a button for sort
+// TODO: create a button for Add 
+// TODO: create a card element to hold the games accordian
+// TODO: create an accordian of games 
+// TODO: on each div within the accordian create 3 rating sections with 5 star values (content, replay, value)
 function Game() {
   return (
   <div>
