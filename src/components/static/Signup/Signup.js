ddc645b8fa6b4e1c97f4c12fdcc633845e11fc9a
maximusDecimalusMeridius <106376888+maximusDecimalusMeridius@users.noreply.github.com>
2023-03-09 15:07:50 -0800

    Added basic styling to login and signup components

--- src/components/static/Signup/Signup.js
+++ src/components/static/Signup/Signup.js
@@ -4,7 +4,14 @@ import "./Signup.css"
 function Signup() {
     return (
         <div className="signupContainer">
-            
+            <div className="signupContainer">
+            <form id="signupForm">
+                <input id="signupUsername" required></input>
+                <input id="signupPassword" required></input>
+                <input id="signupConfirm" required></input>
+            </form>
+            <div className="signupBuffer"></div>
+        </div>
         </div>
     );
 }
