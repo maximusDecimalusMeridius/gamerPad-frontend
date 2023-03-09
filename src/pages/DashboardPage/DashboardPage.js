da7618dd7e971e841d42644e02aea05642f1b070
Kairo97 <109006341+kairo97@users.noreply.github.com>
2023-03-08 21:52:12 -0800

    renamed Communities to CommunitiesPage and moved directory into pages directory

--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -14,9 +14,9 @@ import NotesList from "../../components/dynamic/NotesList/NotesList";
 // only the first online, or most recently online users to populate on friend's list
 // when friend's list is clicked it will take user to full friend's list, clicking on individual user on list will take you to their page
 // TODO: Import dependencies (required files and needed packages)
-// TODO: Create Div social, games, and notes (navbar? just A tags? buttons?)
 // TODO: Create Div for carosel, with A tags on each image within carosel, have background linked to communities page
-// TODO: Create card element for friend's list (top is linked to full friend's list, each friend on list is linked to their own pages)
+// TODO: conditionally rendered friend list, games list, note list
+// TODO: Create card element for friend's list each friend on list is linked to their own pages)
 function DashboardPage() {
     return(
         <div>
