e69d34e3d5fd1723b6ac1ca9bd6869d6911216ce
Maya Lynn <mayalynncohen@gmail.com>
2023-03-13 00:56:55 -0700

    started share notes

--- src/components/dynamic/Note/Note.js
+++ src/components/dynamic/Note/Note.js
@@ -5,10 +5,13 @@ import "../NotesList/NotesList.css";
 function Note({ id, title, textContent, color, createdAt, index, handleDelete, author, currentNotes, handleEdit }) {
 
     const [sideBarVisibility, setsideBarVisibility] = useState('none')
+    const [shareModalVisibility, setShareModalVisibility] = useState('none')
     const [isEditing, setIsEditing] = useState(false)
 
     const [titleInput, setTitleInput] = useState(title)
     const [textInput, setTextInput] = useState(textContent)
+
+    const [friendInput, setFriendInput] = useState("")
 
     const handleMoreClick = (event) => {
         if (sideBarVisibility == 'none') {
@@ -23,11 +26,13 @@ function Note({ id, title, textContent, color, createdAt, index, handleDelete, a
             setTitleInput(e.target.value)
         } else if (e.target.className === "noteText") {
             setTextInput(e.target.value)
+        } else if (e.target.className === "friendInput"){
+            setFriendInput(e.target.value)
         }
     }
 
     const renderNotes = () => {
-        if (currentNotes === "sharedNotes")  {
+        if (currentNotes === "sharedNotes") {
 
             return (
                 <div className="noteCard" key={index} id={`wNote-${index + 1}`} style={{ border: `3px solid ${color}` }}>
@@ -49,15 +54,25 @@ function Note({ id, title, textContent, color, createdAt, index, handleDelete, a
                         <h2 className="noteTitle">{title}</h2>
                         <p className="noteMenu" onClick={handleMoreClick}>...</p>
                         <div className="noteMoreDiv" style={{ background: `${color}`, display: `${sideBarVisibility}` }}>
-                            <p className="noteMoreBtns" onClick={() => setIsEditing(true)}>✏️</p>
+                            <p className="noteMoreBtns" onClick={() => {setIsEditing(true)}}>✏️</p>
                             <p className="noteMoreBtns" onClick={() => { handleDelete(id); setsideBarVisibility('none') }}>🗑️</p>
-                            <p className="noteMoreBtns">🔗</p>
+                            <p className="noteMoreBtns" onClick={() => {setShareModalVisibility('flex'); setsideBarVisibility('none') }}>🔗</p>
                         </div>
+                    </div>
+                    <div className="clickOutDiv" style={{display: `${sideBarVisibility}` }}  onClick={() => {setsideBarVisibility('none') }}>
                     </div>
                     <div className="noteContent">
                         <p>{textContent}</p>
                         <br></br>
                         <p className="bottomNote">On {createdAt.slice(0, 10)}</p>
+                    </div>
+                    <div className="shareModal" style={{ display: `${shareModalVisibility}` }}>
+                        <div className="shareDiv">
+                            <h2>What friend would you like to share this note with?</h2>
+                            <p className="shareNoteCloseBtn" onClick={() => setShareModalVisibility('none')}>X</p>
+                            <input className={"friendInput"} value={friendInput} onChange={handleChange} placeholder={"Friend Username"}></input>
+                            <button>Share Note</button>
+                        </div>
                     </div>
                 </div>
             )
@@ -71,15 +86,15 @@ function Note({ id, title, textContent, color, createdAt, index, handleDelete, a
                             setTitleInput(title);
                             setTextInput(textContent);
                             setsideBarVisibility("none")
-                            }}>X</p>
+                        }}>X</p>
                     </div>
                     <div className="noteContent">
                         <textarea className="noteText" value={textInput} onChange={handleChange}></textarea>
-                        <button  onClick={() => { 
-                            handleEdit(id, titleInput, textInput); 
+                        <button onClick={() => {
+                            handleEdit(id, titleInput, textInput);
                             setsideBarVisibility('none');
                             setIsEditing(false);
-                             }}>Update</button>
+                        }}>Update</button>
                     </div>
                 </div>
             )
