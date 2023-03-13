580790c68d6d0d1ffef0bc45daf950fe33e49631
OneDeadFox <tjosephb.pro@gmail.com>
2023-03-13 07:42:24 -0700

    added share note functionality

--- src/components/dynamic/NotesList/NotesList.js
+++ src/components/dynamic/NotesList/NotesList.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 import SearchBar from "../../static/SearchBar/SearchBar"
 import "./NotesList.css";
 
-function NotesList({writtenNotes, setWrittenNotes, sharedNotes, setSharedNotes, originalWrittenNotesList, setOriginalWrittenNotesList}) {
+function NotesList({ writtenNotes, setWrittenNotes, sharedNotes, setSharedNotes, originalWrittenNotesList, setOriginalWrittenNotesList }) {
     const [originalSharedNotesList, setOriginalSharedNotesList] = useState([])
 
     const [currentNotes, setCurrentNotes] = useState("writtenNotes");
+    const [selectedNote, setSelectedNote] = useState(null);
+    const [friendSearch, setFriendSearch] = useState('')
 
     // useEffect hook to fetch all notes on page load
     useEffect(() => {
@@ -26,7 +28,7 @@ function NotesList({writtenNotes, setWrittenNotes, sharedNotes, setSharedNotes, 
             })
 
             const data = await result.json();
-
+            console.log(data)
             setWrittenNotes(data.WrittenNotes);
             setOriginalWrittenNotesList(data.WrittenNotes)
             setSharedNotes(data.SharedNotes);
@@ -44,29 +46,84 @@ function NotesList({writtenNotes, setWrittenNotes, sharedNotes, setSharedNotes, 
         } else if (event.target.value === "sharedNotes") {
             setSharedNotes([]);
             setCurrentNotes("sharedNotes");
+        } else if (event.target.name === "friendSearch") {
+            setFriendSearch(event.target.value);
         }
     }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const token = localStorage.getItem("token");
+            const result = await fetch(`http://localhost:3001/api/notes/${selectedNote}/shareWith/${friendSearch}`, {
+                method: "POST",
+                headers: {
+                    authorization: token ? `Bearer ${token}` : ''
+                }
+            })
+
+            const data = await result.json();
+            console.log(data)
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const selectNote = (e) => {
+        const currentNote = e.target.parentElement;
+        console.log(currentNote);
+        setSelectedNote(currentNote.getAttribute(`value`));
+        if(selectedNote) {
+            const allNotes = document.querySelectorAll(`.noteCard`);
+            allNotes.forEach(note => {
+                const color = note.getAttribute(`color`);
+                console.log(color);
+                note.setAttribute(`style`, `border: 3px solid ${color}`)
+            })
+            const tempNoteStyle = currentNote.getAttribute(`style`);
+            currentNote.setAttribute(`style`, `${tempNoteStyle}; box-shadow: 0px 0px 8px rgb(104, 255, 157);`);
+            
+        }
+    }
+
     let wNotes;
     console.log(writtenNotes)
-    if(writtenNotes !== [] && writtenNotes !== undefined){
+    if (writtenNotes) {
         wNotes = writtenNotes.map((note, index) => {
             return (
-                <div className="noteCard" key={index} id={`wNote-${index + 1}`} style={{ border: `3px solid ${note.color}` }}>
+                <div
+                    className="noteCard"
+                    key={index}
+                    id={`wNote-${index + 1}`}
+                    color={note.color}
+                    style={{ border: `3px solid ${note.color}` }}
+                    value={`${note.id}`}
+                    onClick={selectNote}
+                >
                     <div className="noteHeader" style={{ background: `${note.color}` }}>
                         <h1 className="noteTitle">{note.title}</h1>
                         <p className="noteDate">{note.createdAt.slice(0, 10)}</p>
                     </div>
-                    <div className="noteContent">{note.textContent}</div>
+                    <div className="noteContent" value={note.id}>{note.textContent}</div>
                 </div>
             )
         })
     }
 
     let sNotes;
-    if(sharedNotes !== [] && sharedNotes !== undefined){
+    if (sharedNotes !== [] && sharedNotes !== undefined) {
         sNotes = sharedNotes.map((note, index) => {
+            console.log(note);
             return (
-                <div className="noteCard" key={index} id={`sNote-${index + 1}`} style={{ border: `3px solid ${note.color}` }}>
+                <div
+                    className="noteCard"
+                    key={index}
+                    id={`sNote-${index + 1}`}
+                    color={note.color}
+                    style={{ border: `3px solid ${note.color}` }}
+                    value={`${note.id}`}
+                    onClick={selectNote}
+                >
                     <div className="noteHeader" style={{ background: `${note.color}` }}>
                         <h1 className="noteTitle">{note.title}</h1>
                         <p className="noteDate">{note.createdAt.slice(0, 10)}</p>
@@ -87,7 +144,7 @@ function NotesList({writtenNotes, setWrittenNotes, sharedNotes, setSharedNotes, 
                 <div className="notes">{sNotes}</div>
             )
         } else {
-            return(
+            return (
                 <div></div>
             )
         }
@@ -107,7 +164,9 @@ function NotesList({writtenNotes, setWrittenNotes, sharedNotes, setSharedNotes, 
     }
 
     const shareNote = () => {
-        
+        if (selectedNote) {
+            document.querySelector(`#findFriendForm`).classList.remove('hidden');
+        }
     }
 
     // TODO: filter data from data array
@@ -127,9 +186,23 @@ function NotesList({writtenNotes, setWrittenNotes, sharedNotes, setSharedNotes, 
                         <label htmlFor="sharedNotes">Shared With You</label>
                     </div>
                     <div>
-                        <button onClick={shareNote}></button>
+                        <button onClick={shareNote}>Share Note</button>
                     </div>
                 </div>
+                <form 
+                    id="findFriendForm" 
+                    className="hidden" 
+                    onSubmit={handleSubmit}
+                >
+                    <input 
+                        id="friendSearch" 
+                        name="friendSearch" 
+                        onChange={handleChange} 
+                        value={friendSearch}
+                    />
+                    <label htmlFor="friendSearch">Find a friend by Id</label>
+                    <button className="submitButton">Send</button>
+                </form>
                 {renderNotes()}
             </div>
         </div>
